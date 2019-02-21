bd7689892b1e2196b6defcb3f3eeab1c403947f2
tezrivera10 <47424669+tezrivera10@users.noreply.github.com>
2019-02-21 15:51:18 -0600

    Add files via upload

---
+++ time2labactivity.py
@@ -0,0 +1,9 @@
+#Raul Rivera
+str_time = input("What time is it now?")
+str_wait_time = input("What is the number of hours to wait?")
+time = input(str_time)
+print(time)
+wait_time = input(str_wait_time)
+print(wait_time)
+time_when_alarm_go_off = time + wait_time
+print(time_when_alarm_go_off)
